d08074a1194b31164bceb9820f94c03e5ae3861b
RegyN <tomekkomisarczyk@gmail.com>
2017-12-15 09:28:19 +0100

    Poprawiłem nieudany wczesniejszy merge

--- main keras.py
+++ main keras.py
@@ -23,9 +23,6 @@ def zapisz_historie(dane_hist, sciezka):
         writer_csv.writerow([row])
 
 
-<<<<<<< HEAD
-def przygotuj_dane_wej():
-=======
 def wsp_sxy_reg_lin(dane):
     ret = 0
     for i, tick in enumerate(dane):
@@ -45,34 +42,29 @@ def wsp_a_reg_lin(dane):
     return ((n * wsp_sxy_reg_lin(dane)) - ((n * (n + 1) / 2) * wsp_sy_reg_lin(dane))) / (n * n * ((5 * n * n) - 1) / 12)
 
 
-def zrob_jeden_trening(l_warstw=2, l_komorek_lstm=20, bias='true', l_komorek_we=80, l_komorek_wy=80,
-                       aktywacja_przejsc='linear', learning_rate=0.15, momentum=0.15, decay=0.0, batch_size=60,
-                       l_epok=1, val_split=0.2, offset=24, trybwartosci=True):
->>>>>>> e1c289bcded9f16a8e696f81015b99396f720e67
+def zrob_jeden_trening(l_warstw=2, l_kom_lstm=20, bias='true', l_komorek_we=80,
+                       l_komorek_wy=80, akt_przejsc='linear', learn_rate=0.15, momentum=0.15, decay=0.0,
+                       batch_size=60, l_epok=1, val_split=0.2, trybwartosci=True):
     sciezka_csv = ".\gielda.csv"
     dane = dc.wczytaj_csv(sciezka_csv)
     dane = dc.konwertuj_na_liczby(dane)
     dane = dc.normalizuj_dane(dane)
     dane = dc.dodaj_ruchoma_srednia(dane, 12)
     dane = dc.dodaj_ruchoma_srednia(dane, 30)
-<<<<<<< HEAD
     dane_test, dane_tren = dc.przygotuj_dane_tren_i_test(dane, offset=24)
-=======
-    dane_test, dane_tren = dc.przygotuj_dane_tren_i_test(dane, offset=offset)
 
->>>>>>> e1c289bcded9f16a8e696f81015b99396f720e67
     tren_input = []
     tren_output = []
     dlug_pak = 720
     for i, pakiet in enumerate(dane_tren):
         pakiet_in = pakiet[0:dlug_pak]
-<<<<<<< HEAD
+
         pakiet_out = [pakiet[dlug_pak + 24 - 1][1],
                       pakiet[dlug_pak + 2 * 24 - 1][1],
                       pakiet[dlug_pak + 3 * 24 - 1][1],
                       pakiet[dlug_pak + 4 * 24 - 1][1],
                       pakiet[dlug_pak + 5 * 24 - 1][1]]
-=======
+
         if trybwartosci == True:
             pakiet_out = [pakiet[dlug_pak + 24 - 1][1],
                           pakiet[dlug_pak + 2 * 24 - 1][1],
@@ -93,15 +85,8 @@ def zrob_jeden_trening(l_warstw=2, l_komorek_lstm=20, bias='true', l_komorek_we=
             else:
                 trend = 1
             pakiet_out = trend
->>>>>>> e1c289bcded9f16a8e696f81015b99396f720e67
         tren_input.append(pakiet_in)
         tren_output.append(pakiet_out)
-    return tren_input, tren_output
-
-
-def zrob_jeden_trening(tren_in, tren_out, l_warstw=2, l_kom_lstm=20, bias='true', l_komorek_we=80,
-                       l_komorek_wy=80, akt_przejsc='linear', learn_rate=0.15, momentum=0.15, decay=0.0,
-                       batch_size=60, l_epok=1, val_split=0.2):
 
     l_wejsc_sieci = 4
     l_wyjsc_sieci = 5
@@ -111,13 +96,8 @@ def zrob_jeden_trening(tren_in, tren_out, l_warstw=2, l_kom_lstm=20, bias='true'
     model.add(Dense(units=l_komorek_we, input_shape=(720, l_wejsc_sieci,), activation=akt_przejsc))
 
     # Dodaję opcjonalne warstwy LSTM
-<<<<<<< HEAD
     for i in range(0, l_warstw-1):
         model.add(LSTM(units=l_kom_lstm, return_sequences=True, use_bias=bias))
-=======
-    for i in range(0, l_warstw - 1):
-        model.add(LSTM(units=l_komorek_lstm, return_sequences=True, use_bias=bias))
->>>>>>> e1c289bcded9f16a8e696f81015b99396f720e67
     # Ostatnia warstwa LSTM
     model.add(LSTM(units=l_kom_lstm, return_sequences=False, use_bias=bias))
     # Dodaję warstwy przejściowe, dostosowujące liczbę wyjść
@@ -129,39 +109,18 @@ def zrob_jeden_trening(tren_in, tren_out, l_warstw=2, l_kom_lstm=20, bias='true'
 
     # Trenuję sieć
     history = LossHistory()
-    model.fit(tren_in, tren_out, batch_size=batch_size, epochs=l_epok, validation_split=val_split,
+    model.fit(tren_input, tren_output, batch_size=batch_size, epochs=l_epok, validation_split=val_split,
               verbose=1, callbacks=[history])
-<<<<<<< HEAD
     # predicted = model.predict(tren_in)
     # print(predicted[15])
     # print(tren_out[15])
     nazwa = "W"+str(l_warstw)+"K"+str(l_kom_lstm)+"LR"+str(learn_rate)+"M"+str(momentum)+"B"+str(batch_size)\
-=======
-    predicted = model.predict(tren_input)
-    print(predicted[15])
-    print(tren_output[15])
-    nazwa = "W" + str(l_warstw) + "K" + str(l_komorek_lstm) + "LR" + str(learning_rate) + "M" + str(
-        momentum) + "B" + str(batch_size) \
->>>>>>> e1c289bcded9f16a8e696f81015b99396f720e67
-            + ".csv"
+             + ".csv"
     zapisz_historie(history, nazwa)
 
 
-<<<<<<< HEAD
-def main() :
-    tren_input, tren_output = przygotuj_dane_wej()
-    zrob_jeden_trening(tren_in=tren_input, tren_out=tren_output, l_warstw=1, l_epok=2)
-    zrob_jeden_trening(tren_in=tren_input, tren_out=tren_output, l_warstw=1, l_epok=2, learn_rate=0.3, momentum=0.3)
-    zrob_jeden_trening(tren_in=tren_input, tren_out=tren_output, l_warstw=1, l_epok=2, learn_rate=0.3, momentum=0.1)
-    zrob_jeden_trening(tren_in=tren_input, tren_out=tren_output, l_warstw=1, l_epok=4, learn_rate=0.05, momentum=0.05)
-    zrob_jeden_trening(tren_in=tren_input, tren_out=tren_output, l_warstw=1, l_kom_lstm=40, l_epok=2)
-    zrob_jeden_trening(tren_in=tren_input, tren_out=tren_output, l_warstw=2, l_kom_lstm=15, l_epok=2)
-    zrob_jeden_trening(tren_in=tren_input, tren_out=tren_output, l_warstw=2, l_kom_lstm=30, l_epok=2)
-=======
-
 def main():
-    zrob_jeden_trening(l_warstw=2, l_komorek_lstm=20, offset=25, aktywacja_przejsc='linear', l_epok=1, trybwartosci=True)
->>>>>>> e1c289bcded9f16a8e696f81015b99396f720e67
+    zrob_jeden_trening(l_warstw=2, l_kom_lstm=20, akt_przejsc='linear', l_epok=1, trybwartosci=True)
 
 
 main()
