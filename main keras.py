e65a87ee7b96716ed92968e3777bf17ff18c7791
bartekx <bartek.z0@wp.pl>
2017-12-19 02:43:34 +0100

    poprawki

--- main keras.py
+++ main keras.py
@@ -45,17 +45,17 @@ def wsp_a_reg_lin(dane):
     return ((n * wsp_sxy_reg_lin(dane)) - ((n * (n + 1) / 2) * wsp_sy_reg_lin(dane))) / (n * n * ((5 * n * n) - 1) / 12)
 
 
-def zrob_jeden_trening(l_warstw=2, l_kom_ukr=20, bias='true', l_komorek_we=80,
-                       l_komorek_wy=80, akt_przejsc='linear', learn_rate=0.15, momentum=0.15, decay=0.0,
+def zrob_jeden_trening(l_warstw=2, l_kom_ukr=20, bias='true', l_komorek_we=20,
+                       akt_przejsc='linear', learn_rate=0.15, momentum=0.15, decay=0.0,
                        batch_size=60, l_epok=1, val_split=0.2, trybwartosci=True, typ='lstm'):
     sciezka_csv = ".\gielda.csv"
     tren_input, tren_output = zrob_dane(sciezka_csv, trybwartosci)
 
     l_wejsc_sieci = 2
     if trybwartosci:
-        l_wyjsc_sieci = 5
-    else:
         l_wyjsc_sieci = 1
+    else:
+        l_wyjsc_sieci = 5
 
     # Tworzę model sieci
     if typ == 'lstm':
@@ -74,24 +74,33 @@ def zrob_jeden_trening(l_warstw=2, l_kom_ukr=20, bias='true', l_komorek_we=80,
     stopper = EarlyStopping(patience=2)
     model.fit(tren_input, tren_output, batch_size=batch_size, epochs=l_epok, validation_split=val_split,
               verbose=1, callbacks=[history, stopper])
-    predicted = model.predict(tren_input)
+    if trybwartosci:
+        predicted = model.predict(tren_input)
+    else:
+        predicted = model.predict_classes(tren_input)
     print(predicted[15])
     print(tren_output[15])
     # print(tren_input[15][-1][1])
-    nazwa = (typ+"W"+str(l_warstw)+"K"+str(l_kom_ukr)+"LR"+str('%.2F' % learn_rate)+"M"+str('%.2F' % momentum)
-             + "B"+str(batch_size)+"A"+akt_przejsc)
+    if trybwartosci:
+        nazwa = (typ + "W" + str(l_warstw) + "K" + str(l_kom_ukr) + "LW" + str(l_komorek_we) + "LR" + str('%.2F' % learn_rate) + "M" + str(
+            '%.2F' % momentum)
+                 + "B" + str(batch_size) + "A" + akt_przejsc + "LE" + str(l_epok) + "W")
+    else:
+        nazwa = (typ + "W" + str(l_warstw) + "K" + str(l_kom_ukr) + "LW" + str(l_komorek_we) + "LR" + str('%.2F' % learn_rate) + "M" + str(
+            '%.2F' % momentum)
+                 + "B" + str(batch_size) + "A" + akt_przejsc + "LE" + str(l_epok) + "T")
     zapisz_historie(history, nazwa+".csv")
     mu.zapisz_model(model, nazwa+".h5")
 
 
-def zrob_model_lstm(akt_przejsc, bias, l_kom_lstm, l_komorek_we, l_warstw, l_wejsc_sieci, l_wyjsc_sieci):
+def zrob_model_lstm(akt_przejsc, bias, l_kom_ukr, l_kom_lstm, l_warstw, l_wejsc_sieci, l_wyjsc_sieci):
     model = keras.Sequential()
     # model.add(Dense(units=l_komorek_we,  activation=akt_przejsc))
-    model.add(LSTM(input_shape=(720, l_wejsc_sieci,), units=l_kom_lstm, return_sequences=True, use_bias=bias))
+    model.add(LSTM(input_shape=(720, l_wejsc_sieci,), units=l_kom_lstm, return_sequences=True, use_bias=bias, activation=akt_przejsc))
     for i in range(0, l_warstw - 1):
-        model.add(LSTM(units=l_kom_lstm, return_sequences=True, use_bias=bias))
+        model.add(LSTM(units=l_kom_ukr, return_sequences=True, use_bias=bias, activation=akt_przejsc))
     # Dodaję warstwy przejściowe, dostosowujące liczbę wyjść
-    model.add(LSTM(units=5, return_sequences=False, use_bias=bias))
+    model.add(LSTM(units=l_wyjsc_sieci, return_sequences=False, use_bias=bias, activation=akt_przejsc))
     # model.add(Dense(units=l_wyjsc_sieci, activation=akt_przejsc))
     return model
 
@@ -101,7 +110,7 @@ def zrob_model_rnn(akt_przejsc, bias, l_kom_ukr, l_komorek_we, l_warstw, l_wejsc
     model.add(Dense(units=l_komorek_we, input_shape=(720, l_wejsc_sieci,), activation=akt_przejsc))
     # Dodaję opcjonalne warstwy LSTM
     for i in range(0, l_warstw - 1):
-        model.add(SimpleRNN(units=l_kom_ukr, return_sequences=True, use_bias=bias, activation="tanh"))
+        model.add(SimpleRNN(units=l_kom_ukr, return_sequences=True, use_bias=bias, activation=akt_przejsc))
     # Ostatnia warstwa LSTM
     model.add(SimpleRNN(units=l_kom_ukr, return_sequences=False, use_bias=bias))
     # Dodaję warstwy przejściowe, dostosowujące liczbę wyjść
@@ -125,25 +134,19 @@ def zrob_dane(sciezka_csv, trybwartosci):
         for j in range(0, dlug_pak):
             pakiet_in.append([pakiet[j][0], pakiet[j][1]])
         if trybwartosci:
-            pakiet_out = [pakiet[dlug_pak + 24 - 1][1],
-                          pakiet[dlug_pak + 2 * 24 - 1][1],
-                          pakiet[dlug_pak + 3 * 24 - 1][1],
-                          pakiet[dlug_pak + 4 * 24 - 1][1],
-                          pakiet[dlug_pak + 5 * 24 - 1][1]]
+            pakiet_out = pakiet[dlug_pak + 5 * 24 - 1][1]
         else:
             a = wsp_a_reg_lin(pakiet)
-            trend = 0
             if a > 1 / 20000:
-                trend = 5
+                pakiet_out = [0,0,0,0,1]
             elif a > 1 / 200000:
-                trend = 4
+                pakiet_out = [0, 0, 0, 1, 0]
             elif a > -1 / 200000:
-                trend = 3
+                pakiet_out = [0, 0, 1, 0, 0]
             elif a > -1 / 20000:
-                trend = 2
+                pakiet_out = [0, 1, 0, 0, 0]
             else:
-                trend = 1
-            pakiet_out = trend
+                pakiet_out = [1, 0, 0, 0, 0]
         tren_input.append(np.array(pakiet_in))
         tren_output.append(np.array(pakiet_out))
     return np.array(tren_input), np.array(tren_output)
@@ -171,7 +174,7 @@ def main():
     wybor = ""
     while True:
         print("")
-        wybor = input("1- uczenie sieci 2- wczytanie wykresu 0- wyjscie z programu")
+        wybor = input("1- uczenie sieci 2- wczytanie wykresu 3 - uruchom skrypt 4 - rysuj wykresy skryptu 0- wyjscie z programu")
         if int(wybor) == 1:
             zrob_jeden_trening(l_warstw=1, l_kom_ukr=20, akt_przejsc='sigmoid', l_epok=15, trybwartosci=True,
                                l_komorek_wy=100, l_komorek_we=20, typ='lstm', learn_rate=0.5, momentum=0.5)
@@ -179,6 +182,41 @@ def main():
             sciezka = input("Podaj nazwe pliku do wczytania")
             a = wczytaj_wykres(sciezka)
             rysuj_wykres(a)
+        elif int(wybor) == 3:
+            zrob_jeden_trening(l_warstw=4, l_kom_ukr=20, bias='true', l_komorek_we=32,
+                               akt_przejsc='tanh', learn_rate=0.15, momentum=0.15, decay=0.0,
+                               batch_size=60, l_epok=2, val_split=0.2, trybwartosci=False, typ='lstm')
+            zrob_jeden_trening(l_warstw=1, l_kom_ukr=20, bias='true', l_komorek_we=32,
+                               akt_przejsc='tanh', learn_rate=0.15, momentum=0.15, decay=0.0,
+                               batch_size=60, l_epok=2, val_split=0.2, trybwartosci=False, typ='rnn')
+            if False:
+                zrob_jeden_trening(l_warstw=4, l_kom_ukr=20, bias='true', l_komorek_we=32,
+                                   akt_przejsc='tanh', learn_rate=0.15, momentum=0.15, decay=0.0,
+                                   batch_size=60, l_epok=2, val_split=0.2, trybwartosci=True, typ='lstm')
+                zrob_jeden_trening(l_warstw=1, l_kom_ukr=20, bias='true', l_komorek_we=32,
+                                   akt_przejsc='tanh', learn_rate=0.15, momentum=0.15, decay=0.0,
+                                   batch_size=60, l_epok=2, val_split=0.2, trybwartosci=True, typ='rnn')
+                zrob_jeden_trening(l_warstw=1, l_kom_ukr=20, bias='true', l_komorek_we=4,
+                                   akt_przejsc='tanh', learn_rate=0.15, momentum=0.15, decay=0.0,
+                                   batch_size=60, l_epok=2, val_split=0.2, trybwartosci=True, typ='lstm')
+                zrob_jeden_trening(l_warstw=4, l_kom_ukr=20, bias='true', l_komorek_we=32,
+                                   akt_przejsc='tanh', learn_rate=0.3, momentum=0.5, decay=0.0,
+                                   batch_size=60, l_epok=2, val_split=0.2, trybwartosci=True, typ='lstm')
+                zrob_jeden_trening(l_warstw=4, l_kom_ukr=50, bias='true', l_komorek_we=15,
+                                   akt_przejsc='tanh', learn_rate=0.15, momentum=0.15, decay=0.0,
+                                   batch_size=30, l_epok=1, val_split=0.2, trybwartosci=True, typ='lstm')
+                zrob_jeden_trening(l_warstw=15, l_kom_ukr=15, bias='true', l_komorek_we=4,
+                                   akt_przejsc='tanh', learn_rate=0.15, momentum=0.3, decay=0.0,
+                                   batch_size=60, l_epok=2, val_split=0.2, trybwartosci=True, typ='lstm')
+        elif int(wybor) == 4:
+            rysuj_wykres(wczytaj_wykres(".\logs\lstmW4K20LW32LR0.15M0.15B60AtanhLE2W.csv"))
+            rysuj_wykres(wczytaj_wykres(".\logs\lstmW4K20LW32LR0.15M0.15B60AtanhLE2T.csv"))
+            rysuj_wykres(wczytaj_wykres(".\logs\\rnnW1K20LW32LR0.15M0.15B60AtanhLE2W.csv"))
+            rysuj_wykres(wczytaj_wykres(".\logs\\rnnW1K20LW32LR0.15M0.15B60AtanhLE2T.csv"))
+            rysuj_wykres(wczytaj_wykres(".\logs\lstmW1K20LW4LR0.15M0.15B60AtanhLE2W.csv"))
+            rysuj_wykres(wczytaj_wykres(".\logs\lstmW4K20LW32LR0.30M0.50B60AtanhLE2W.csv"))
+            rysuj_wykres(wczytaj_wykres(".\logs\lstmW4K50LW15LR0.15M0.15B30AtanhLE1W.csv"))
+            rysuj_wykres(wczytaj_wykres(".\logs\lstmW15K15LW4LR0.15M0.30B60AtanhLE2W.csv"))
         elif int(wybor) == 0:
             break
         else:
